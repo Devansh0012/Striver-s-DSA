f79fd1e51dc76137db5489c263a9ee692f32d0df
Devansh0012 <devanshdubey0012@gmail.com>
2024-07-22 01:28:40 +0530

    added patterns

---
+++ learncpp.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int age;
+    cin >> age;
+
+    if(age < 18){
+        cout << "Not eligible for job";
+    }
+
+    else if(age <= 57){
+        cout<<"Eligible for job";
+        if(age >=55){
+            cout << ", but retirement soon!";
+        }
+    }
+
+    
+        else{
+            cout <<"retirement time";
+        }
+
+    return 0;
+}
